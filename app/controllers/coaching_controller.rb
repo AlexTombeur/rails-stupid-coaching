2fc101095d7273d415b264ef9e343eb4a0567d29
Alexandre Tombeur <alexandre.tombeur@gmail.com>
2016-08-08 16:08:17 +0200

    message

--- app/controllers/coaching_controller.rb
+++ app/controllers/coaching_controller.rb
@@ -1,11 +1,28 @@
 class CoachingController < ApplicationController
 
-  def ask
+def ask
+ end
 
-  end
-
-  def answer
+ def answer
+   # GET '/answer'
    @input = params[:input]
-  end
+   if @input == "I AM GOING TO WORK RIGHT NOW!"
+     @result =  ""
+   elsif @input == @input.upcase
+     @result = "I can feel your motivation!" + " " + coach_answer(@input)
+   else
+     @result = coach_answer(@input)
+   end
+ end
+
+ def coach_answer(input)
+   if input.include? "?"
+     return "Silly question, get dressed and go to work!"
+   elsif input == "I am going to work right now!"
+     return  ""
+   else
+     return "I don't care, get dressed and go to work!"
+   end
+ end
 
 end
